55f429d12dffce5b830c628555275c746d95bed8
praveennegi420 <praveennegiuk07@gmail.com>
2023-07-01 22:53:50 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const connection = require("./db");
+const app = express();
+
+// connection();
+app.use(cors());
+app.use(express.json());
+
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
