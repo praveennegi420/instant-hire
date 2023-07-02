60581455ca4346735bf16462c90956a3c7043b54
LostLord07 <satishsingh45264209@gmail.com>
2023-07-02 21:01:24 +0530

    filter feature

--- server/routes/jobroutes.js
+++ server/routes/jobroutes.js
@@ -29,6 +29,18 @@ router.get("/applicants/:id", auth,  async (req, res) => {
 	res.status(200).send({ data: users });
 });
 
+router.post("/applicants/:id", auth,  async (req, res) => {
+	const jobid= req.params.id; console.log(jobid)
+	const {price, cgpa, exp} = req.params; 
+	const users = await Applicant.find({jobid:jobid});
+	let newUsers  = users;
+	newUsers = newUsers.filter(user => user.cgpa > cgpa) ;
+	newUsers = newUsers.filter(user=> user.experience > exp); 
+	newUsers = newUsers.filter(user=>user.salaryexpect > price); 
+
+	res.status(200).send({ data: newUsers });
+});
+
 // GET ALL JOBS 
 router.get('/', async(req,res)=>{
 	const jobs= await Job.find({});
