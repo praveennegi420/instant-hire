3a2bf1e6114a4c4a41a2f94935ef4161fc80dfc8
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2023-07-02 13:24:12 +0530

    login signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import Home from './pages/Home'
 import Signup from './pages/signup'
-
+import LoginPage from './pages/login'
 import { Route, Routes } from 'react-router-dom'
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://127.0.0.1:8080';
 function App() {
 
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage/>} />
         <Route path='/signup' element={<Signup/>} />
       </Routes>
     </>
